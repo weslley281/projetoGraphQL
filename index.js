568f7934e9d39e135ff2a1761b806e87480fd13e
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-30 17:13:21 -0400

    a

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
   scalar Date
 
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   #Pontos de entrada da sua API
   type Query {
-    ola: String
-    horaAtual: Date
+    ola: String!
+    horaAtual: Date!
+    usuarioLogado: Usuario
   }
 `;
 
@@ -20,6 +30,16 @@ const resolvers = {
     },
     horaAtual() {
       return hourComplete;
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: 'Weslley Ferraz',
+        email: 'weslleyhenrique800@gmail.com',
+        idade: 26,
+        salario: 2.45,
+        vip: true,
+      };
     },
   },
 };
